a358274c7d5a1d23fce003acca7b454fb1cf7cd2
lujin <syani@qq.com>
2016-09-20 18:47:36 +0800

    原生hadoop测试代码

--- hadoop-traditional/src/main/java/wordcount/JSubmit.java
+++ hadoop-traditional/src/main/java/wordcount/JSubmit.java
@@ -14,14 +14,19 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
+import org.apache.hadoop.util.GenericOptionsParser;
 
 public class JSubmit {
     public static void main(String[] args) throws IOException,
             URISyntaxException, InterruptedException, ClassNotFoundException {
+
+        /**
+
         //Path类为hadoop API定义，创建两个Path对象，一个输入文件的路径，一个输入结果的路径
-        Path outPath = new Path("hdfs://localhost:9000/out");
+        Path outPath = new Path("hdfs://localhost:9000/test");
         //输入文件的路径为本地linux系统的文件路径
-        Path inPath = new Path("/home/hadoop/word");
+        Path inPath = new Path("data/");
         //创建默认的Configuration对象
         Configuration conf = new Configuration();
         //根据地址和conf得到hadoop的文件系统独享
@@ -39,7 +44,8 @@ public class JSubmit {
         job.setInputFormatClass(TextInputFormat.class);
 
         //1.2调用自定义的Mapper类的map方法进行操作
-        //设置处理的Mapper类
+        //设置处理的Mapper类ls
+
         job.setMapperClass(JMapper.class);
         //设置Mapper类处理完毕之后输出的键值对 的 数据类型
         job.setMapOutputKeyClass(Text.class);
@@ -68,5 +74,33 @@ public class JSubmit {
 
         //将当前的job对象提交
         job.waitForCompletion(true);
+         */
+
+        Configuration conf = new Configuration();
+        // 多队列hadoop集群中，设置使用的队列
+        conf.set("mapred.job.queue.name", "regular");
+        // 之所以此处不直接用 argv[1] 这样的，是为了排除掉运行时的集群属性参数，例如队列参数，
+        // 得到用户输入的纯参数，如路径信息等
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        for (String argsStr : otherArgs) {
+            System.out.println("-->> " + argsStr);
+        }
+        if (otherArgs.length < 2) {
+            System.err.println("Usage: wordcount <in> <out>");
+            System.exit(2);
+        }
+        Job job = new Job(conf, "word count");
+        job.setJarByClass(JSubmit.class);
+        // map、reduce 输入输出类
+        job.setMapperClass(JMapper.class);
+        job.setCombinerClass(JReducer.class);
+        job.setReducerClass(JReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+        // 输入输出路径
+        FileInputFormat.addInputPath(job, new Path(otherArgs[1]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[2]));
+        // 多子job的类中，可以保证各个子job串行执行
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
